641f5c08e321c1bb3b9f5216242943a9a5808d09
risulkarimx1 <risulkarimx1@gmail.com>
2019-01-26 21:52:13 +0100

    1. Using Subject

---
+++ IntroToRxBook/Assets/_Scripts/MonoTester.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class MonoTester : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+       //var observer = new MyConsoleObserver<int>();
+       //var numbers = new MySequenceOfNumbers();
+       // numbers.Subscribe(observer);
+       var subject = new Subject<string>();
+       //PrintSequenceToConsole(subject);
+       subject.Subscribe(value => Debug.Log(value));
+       subject.OnNext("a");
+       subject.OnNext("b");
+       subject.OnNext("c");
+    }
+
+        
+    //void PrintSequenceToConsole(IObservable<string> seq)
+    //{
+    //    seq.Subscribe(value=>Debug.Log(value));
+    //}
+    //IObservable<string> DebugLog(string value)
+    //{
+    //   Debug.Log(value);
+    //   return new IObservable<string>();
+    //}
+
+}
---
+++ IntroToRxBook/Assets/_Scripts/MyConsoleObserver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using  UnityEngine;
+public class MyConsoleObserver<T> : IObserver<T>
+{
+    public void OnCompleted()
+    {
+        Debug.Log($"Sequence ended");
+    }
+    public void OnError(Exception error)
+    {
+        Debug.Log($"Sequence failed with error: {error}");
+    }
+    public void OnNext(T value)
+    {
+        Debug.Log($"Received value: {value}");
+    }
+}
