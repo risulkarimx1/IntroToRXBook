65342094fbd6655116952dcb74a260be5de67ff9
risulkarimx1 <risulkarimx1@gmail.com>
2019-01-27 01:14:35 +0100

    Exception handling for OnError method

--- IntroToRxBook/Assets/_Scripts/MonoTester.cs
+++ IntroToRxBook/Assets/_Scripts/MonoTester.cs
@@ -1,20 +1,36 @@
-﻿using UniRx;
+﻿using System;
+using UniRx;
 using UnityEngine;
 
 public class MonoTester : MonoBehaviour
 {
+    
+
     // Start is called before the first frame update
     private void Start()
     {
-        var subject = new AsyncSubject<string>();
-        subject.OnNext("b");
-        subject.OnNext("c");
-        subject.Subscribe(value => Debug.Log(value));
-        subject.OnNext("x");
-        subject.OnNext("y");
-        subject.OnCompleted();
+        var values = new Subject<int>();
+        try
+        {
+            values.Subscribe(
+                value=>Debug.Log(value),
+                ex =>Debug.LogError($"Exception Handled {ex.ToString()}")
+                );
+        }
+        catch (Exception e)
+        {
+            Debug.Log($"wont catch anything here {e.ToString()}");
+        }
+        values.OnNext(0);
+        values.OnError(new Exception("Dummy exception"));
     }
-
+    //var subject = new AsyncSubject<string>();
+    //subject.OnNext("b");
+    //subject.OnNext("c");
+    //subject.Subscribe(value => Debug.Log(value));
+    //subject.OnNext("x");
+    //subject.OnNext("y");
+    //subject.OnCompleted();
 
     //void PrintSequenceToConsole(IObservable<string> seq)
     //{
